6d589250f8ea9ffda98f53a228a0bbb56ba42a97
Soroosh Mirshekari <soroosh.mirshekari@gmail.com>
2025-03-14 10:57:02 +0330

    updated

--- Entities.py
+++ Entities.py
@@ -2,31 +2,19 @@
 from abc import ABC, abstractmethod
 import pygame
 from random import randint
+
 # an Instanst
 class Entity(ABC):
     @abstractmethod
     def __init__(self, border_thickness, width, height, screen):
-        self.__border_thickness = border_thickness
-        self.__width = width
-        self.__height = height
-        self.__screen = screen
+        self._border_thickness = border_thickness
+        self._width = width
+        self._height = height
+        self._screen = screen
 
     @abstractmethod
     def random_placement(self):
         pass
-
-    # Getter methods for private variables
-    def get_border_thickness(self):
-        return self.__border_thickness
-
-    def get_width(self):
-        return self.__width
-
-    def get_height(self):
-        return self.__height
-
-    def get_screen(self):
-        return self.__screen
 
 class Aim(Entity):
     def __init__(self, border_thickness, width, height, screen, speed, size, color, controls):
@@ -39,8 +27,8 @@ class Aim(Entity):
         self.__y = randint(border_thickness, height - border_thickness)
 
     def random_placement(self):
-        self.__x = randint(self.get_border_thickness(), self.get_width() - self.get_border_thickness())
-        self.__y = randint(self.get_border_thickness(), self.get_height() - self.get_border_thickness())
+        self.__x = randint(self._border_thickness, self._width - self._border_thickness)
+        self.__y = randint(self._border_thickness, self._height - self._border_thickness)
 
     def press_key(self, keys):
 
@@ -55,30 +43,11 @@ class Aim(Entity):
             self.__x += self.__speed
 
         # Keep aim within screen bounds
-        self.__x = max(self.get_border_thickness(), min(self.__x, self.get_width() - self.get_border_thickness()))
-        self.__y = max(self.get_border_thickness(), min(self.__y, self.get_height() - self.get_border_thickness()))
+        self.__x = max(self._border_thickness, min(self.__x, self._width - self._border_thickness))
+        self.__y = max(self._border_thickness, min(self.__y, self._height - self._border_thickness))
 
     def draw(self):  # Draw aim
-        pygame.draw.circle(self.get_screen(), self.__color, (int(self.__x), int(self.__y)), self.__size)
+        pygame.draw.circle(self._screen, self.__color, (int(self.__x), int(self.__y)), self.__size)
 
         # Update the display
         pygame.display.flip()
-
-    # Getter methods for private variables
-    def get_speed(self):
-        return self.__speed
-
-    def get_size(self):
-        return self.__size
-
-    def get_color(self):
-        return self.__color
-
-    def get_controls(self):
-        return self.__controls
-
-    def get_x(self):
-        return self.__x
-
-    def get_y(self):
-        return self.__y
